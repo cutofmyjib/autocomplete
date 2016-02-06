c92cef33f24e0dd4246ef1309cc7b8cf8d254e82
cutofmyjib <thedianalozano@gmail.com>
2016-02-06 00:22:59 -0800

    use textcomplete library

--- autocomplete.js
+++ autocomplete.js
@@ -1,36 +1,27 @@
-$( document ).ready(
-  $("#commentbox").keypress(function(event){
-    if (event.which === 64) {
-      $("#commentbox").on('keydown', function(event){
-        var index = event.target.value.indexOf('@');
-        var target = event.target.value.slice(index + 1);
-        if (!(target === ' ')) {
-          $.ajax({
-            url: "./data.json",
-            dataType: "json",
-            success: function(response){
-              $("#users").empty();
-              var matches = findUser(target, response);
-              for (var i = 0; i < matches.length; i++) {
-                $("#users").append("<p>" + matches[i].username + " - " + matches[i].name + "</p>")
-              }
-            }
-          })//end ajax
+$( document ).ready(function() {
+  $.ajax({
+    url: "./data.json",
+    dataType: "json",
+    success: function(response){
+      $("#commentbox").textcomplete([
+      {
+        users: response,
+        match: /\B@(\w*)$/,
+        search: function (input, callback) {
+          input = new RegExp(input, 'i');
+          callback($.map(this.users, function (user) {
+            return (( user.username.match(input)) || (user.name.match(input)))
+                      ? user.username + ' - ' + user.name
+                      : null;
+          }));
+        },
+        index: 1,
+        replace: function (user) {
+          user = user.split(' - ');
+          return '@' + user[0] + ' ';
         }
-      })
+      }
+    ], { appendTo: "#users" })
     }
-
-  })
-
-)
-
-function findUser(input, data) {
-  var re = new RegExp(input, 'i');
-  var matches = [];
-  for (var i = 0; i < data.length; i++) {
-    if ((data[i].name.match(re)) || (data[i].username.match(re))) {
-      matches.push(data[i]);
-    }
-  }
-  return matches;
-}
+  })//end ajax
+})
